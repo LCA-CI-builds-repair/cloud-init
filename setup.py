36fe43898f92cde7dd7b9245187a479299bb89a4
ubuntu <ubuntu@ip-10-0-0-215>
2024-05-02 11:46:18 +0000

    test_lca_code_editing__code-editing-metrics-cdfcab645d0a57a__id_24

--- setup.py
+++ setup.py
@@ -260,9 +260,8 @@ class InitsysInstallData(install):
                 if not files:
                     continue
                 self.distribution.data_files.append((INITSYS_ROOTS[k], files))
-        # Force that command to reinitialize (with new file list)
-        self.distribution.reinitialize_command("install_data", True)
-
+# Force the command to reinitialize with a new file list
+self.distribution.reinitialize_command("install_data", force=True)
 
 if not in_virtualenv():
     USR = "/" + USR
